a15481719f23e127ca43470b70ae4b1527e195d4
heqingying <heqingying@feiniu.com>
2022-09-21 16:40:44 +0800

    fix: remove stylelint

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const pkg = require('../package.json');
 const _eslint = require('./eslint');
-const _stylelint = require('./stylelint');
 
 export const cliVersion: string = pkg.version;
 export const eslint = _eslint;
-export const stylelint = _stylelint;
